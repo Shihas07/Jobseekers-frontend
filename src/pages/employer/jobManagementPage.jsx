55abdbf601adeb41c03758a50d456a03eaabe358
shihas07 <shihas732@gmail.com>
2024-10-03 17:51:53 +0530

    updations

--- src/pages/employer/jobManagementPage.jsx
+++ src/pages/employer/jobManagementPage.jsx
@@ -1,13 +1,14 @@
-
-
-import React from 'react'
+import { AddBox } from "@mui/icons-material";
+import { Box, Button } from "@mui/material";
+import { Flex } from "antd";
+import React from "react";
 
 export default function JobManagementPage() {
   return (
     <div>
-       this is job page
+      <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+        <Button variant="contained">Add Job</Button>
+      </Box>
     </div>
-  )
+  );
 }
-
-
